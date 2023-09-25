b18c7be2f9d44828bdf8b8191675b7c848961ab1
Ivan Izobov <izobov9@gmail.com>
2023-09-25 10:59:34 +0300

    Info banner

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,7 +4,12 @@ import "../app.css";
   import Wallet from "./Wallet.svelte";
 
 </script>
-<Banner position="absolute" bannerType="cta" > Be carfull and use only localhost:8545! Do not send any real tokens to this smart contract  </Banner>
+<Banner position="absolute" bannerType="cta" ><div >
+
+      <p class="font-bold">Be careful: Use only localhost:8545!</p>
+      <p>Do not send any real tokens to this smart contract!</p>
+      <p>Add a new account: 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d (import key)</p>
+  </div></Banner>
 
 <div class="p-24 w-full h-full bg-gray-400">
     <Wallet />
