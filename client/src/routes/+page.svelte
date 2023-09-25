6cfe50c088a7491496960d4b75cfdfcd52ec67b3
Ivan Izobov <izobov9@gmail.com>
2023-09-25 10:42:17 +0300

    Add info banner

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-    import "../app.css";
+    import { Banner } from "flowbite-svelte";
+import "../app.css";
   import Wallet from "./Wallet.svelte";
 
 </script>
+<Banner position="absolute" bannerType="cta" > Be carfull and use only localhost:8545! Do not send any real tokens to this smart contract  </Banner>
+
 <div class="p-24 w-full h-full bg-gray-400">
     <Wallet />
 </div>
